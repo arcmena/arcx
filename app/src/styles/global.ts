9f38f7a81d85fa858d306a7e6d41a0ca0af1081b
Marcelo Mena <marcelomena010@gmail.com>
2021-05-29 15:54:09 -0300

    feat: add downloaded fonts

--- app/src/styles/global.ts
+++ app/src/styles/global.ts
@@ -1,10 +1,47 @@
 import { createGlobalStyle } from 'styled-components'
 
+import ExtraLight from 'assets/fonts/Poppins-ExtraLight.ttf'
+import Light from 'assets/fonts/Poppins-Light.ttf'
+import Regular from 'assets/fonts/Poppins-Regular.ttf'
+import Medium from 'assets/fonts/Poppins-Medium.ttf'
+import SemiBold from 'assets/fonts/Poppins-SemiBold.ttf'
+import Bold from 'assets/fonts/Poppins-Bold.ttf'
+
 export default createGlobalStyle`
+    @font-face {
+        font-family: "Poppins";
+        src: url(${ExtraLight}) format("truetype");
+        font-weight: 200;
+    }
+    @font-face {
+        font-family: "Poppins";
+        src: url(${Light}) format("truetype");
+        font-weight: 300;
+    }
+    @font-face {
+        font-family: "Poppins";
+        src: url(${Regular}) format("truetype");
+        font-weight: 400;
+    }
+    @font-face {
+        font-family: "Poppins";
+        src: url(${Medium}) format("truetype");
+        font-weight: 500;
+    }
+    @font-face {
+        font-family: "Poppins";
+        src: url(${SemiBold}) format("truetype");
+        font-weight: 600;
+    }
+    @font-face {
+        font-family: "Poppins";
+        src: url(${Bold}) format("truetype");
+        font-weight: 700;
+    }
+
     :root{
         --navbar-width: 260px
     }
-
     * {
         box-sizing: border-box;
         padding: 0;
