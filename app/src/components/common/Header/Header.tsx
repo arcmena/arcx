49688a869ce48e28ee76d21096037b62bafb45ca
Marcelo Mena <marcelomena010@gmail.com>
2021-05-31 21:18:43 -0300

    chore: add DateSelect on Header

--- app/src/components/common/Header/Header.tsx
+++ app/src/components/common/Header/Header.tsx
@@ -1,21 +1,48 @@
 import { FC } from 'react'
 import styled from 'styled-components'
 
+import { RiCalendarTodoLine } from 'react-icons/ri'
+import { IoChevronDownOutline } from 'react-icons/io5'
+
 import { formatDate } from 'utils/formatters'
 
 const HeaderWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+
     user-select: none;
 
-    h1 {
-        color: ${({ theme: { colors } }) => colors.black[1]};
-        font-size: 3rem;
-        font-weight: 600;
-    }
+    > div {
+        h1 {
+            color: ${({ theme: { colors } }) => colors.black[1]};
+            font-size: 3rem;
+            font-weight: 600;
+        }
 
-    span {
-        color: ${({ theme: { colors } }) => colors.gray[5]};
-        font-size: 1.2rem;
-        font-weight: 700;
+        span {
+            color: ${({ theme: { colors } }) => colors.gray[5]};
+            font-size: 1.2rem;
+            font-weight: 600;
+        }
+    }
+`
+
+const DateSelect = styled.div`
+    display: flex;
+    align-self: center;
+    align-items: center;
+
+    cursor: pointer;
+    border-radius: 1.7rem;
+
+    padding: 1rem 2rem;
+
+    background-color: ${({ theme: { colors } }) => colors.gray[2]};
+
+    h2 {
+        font-weight: 600;
+        font-size: 1.4rem;
+        margin: 0 2rem 0 1rem;
     }
 `
 
@@ -32,6 +59,12 @@ const Header: FC<props> = ({ title }) => {
                 <h1>{title}</h1>
                 <span>{formatDate(dateNow, 'eeee, dd MMMM yyyy')}</span>
             </div>
+
+            <DateSelect>
+                <RiCalendarTodoLine size={22} />
+                <h2>May - Nov 2021</h2>
+                <IoChevronDownOutline size={15} />
+            </DateSelect>
         </HeaderWrapper>
     )
 }
