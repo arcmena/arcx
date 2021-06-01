44c7dde70bec3d98d8823d76c0502bd3e69bdfb7
Marcelo Mena <marcelomena010@gmail.com>
2021-05-31 21:18:09 -0300

    feat: create Section component

---
+++ app/src/components/ui/Section/Section.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import styled from 'styled-components'
+
+const SectionWrapper = styled.section`
+    border-radius: 1.7rem;
+    box-shadow: 0px 0px 148px -26px rgba(0, 1, 5, 0.36);
+    background-color: ${({ theme: { colors } }) => colors.white};
+`
+
+const Section: FC = ({ children }) => {
+    return <SectionWrapper>{children}</SectionWrapper>
+}
+
+export default Section
